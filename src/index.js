9b79ae259d338c899227d009c7438c6369c8fb8a
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-28 09:04:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const refs = {
+//   button: document.querySelector('#btn'),
+//   input: document.querySelector('#user-num-input'),
+// };
+// const randomNum = Math.floor(Math.random() * 100);
+
+// let count = 0;
+
+// refs.button.addEventListener('click', onRandom);
+
+// function onRandom() {
+//   count += 1;
+
+//   if (Number(refs.input.value) > randomNum) {
+//     Notify.failure('Введене число більше');
+//   }
+//   if (Number(refs.input.value) < randomNum) {
+//     Notify.failure('Введене число менше');
+//   }
+//   if (Number(refs.input.value) === randomNum) {
+//     Notify.success('Вгадали');
+//   }
+//   Notify.info(`Кількість спроб: ${count}`);
+// }
 
 const refs = {
   button: document.querySelector('#btn'),
   input: document.querySelector('#user-num-input'),
+  result: document.querySelector('#result'),
 };
 const randomNum = Math.floor(Math.random() * 100);
 
@@ -14,13 +40,16 @@ function onRandom() {
   count += 1;
 
   if (Number(refs.input.value) > randomNum) {
-    Notify.failure('Введене число більше');
+    alert('Введене число більше');
   }
   if (Number(refs.input.value) < randomNum) {
-    Notify.failure('Введене число менше');
+    refs.result.innerHTML += 'Введене число менше';
+    alert('Введене число менше');
   }
   if (Number(refs.input.value) === randomNum) {
-    Notify.success('Вгадали');
+    refs.result.innerHTML += 'Вгадали';
+    alert('Вгадали');
   }
-  Notify.info(`Кількість спроб: ${count}`);
+      alert(`Кількість спроб: ${count}`);
 }
+
